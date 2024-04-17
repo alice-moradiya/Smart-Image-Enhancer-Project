13d4f8dd15d24160d4febfa76cd043d91dc0b3bb
Alice <alice.al.moradiya@gmail.com>
2024-04-16 21:49:46 -0600

    Adding sharpening function

--- enhancement_tool.py
+++ enhancement_tool.py
@@ -37,6 +37,20 @@ def adjust_contrast(image, contrast=1.0):
     contrast_img = cv2.addWeighted(image, alpha_c, image, 0, gamma_c)
     return contrast_img
 
+def sharpen_image(image):
+    if image is None:
+        raise ValueError("Invalid image provided for sharpening.")
+    
+    # Definig a sharpening kernel
+    sharpening_kernel = np.array([[-1,-1,-1], 
+                                  [-1, 9,-1],
+                                  [-1,-1,-1]])
+    
+    # Applying the sharpening kernel to the image using filter2D
+    sharpened_image = cv2.filter2D(image, -1, sharpening_kernel)
+    
+    return sharpened_image
+
 def main():
     img_path = r'C:\Users\PC\Desktop\Smart image enhancer\Smart-Image-Enhancer-Project\image1.jpg'
     image = cv2.imread(img_path)
@@ -45,18 +59,20 @@ def main():
         print("Could not load the image.")
         return
     
-    user_choice = input("Do you want to adjust brightness or contrast? (Enter 'brightness' or 'contrast'): ").lower()
+    user_choice = input("Do you want to adjust brightness or contrast or sharpen? (Enter 'b' or 'c' or 's'): ").lower()
 
-    if user_choice not in ['brightness', 'contrast']:
+    if user_choice not in ['b', 'c', 's']:
         print("Invalid choice. Please enter 'brightness' or 'contrast'.")
         return
     
-    if user_choice == 'brightness':
+    if user_choice == 'b':
         value = int(input("Enter the brightness value (negative to decrease, positive to increase) (ex. -1 or 2 or 3): "))
         adjusted_image = adjust_brightness(image, brightness=value)
-    else:
+    if user_choice == 'c':
         value = float(input("Enter the contrast value (less than 1 to decrease, greater than 1 to increase, 1 for no change): "))
         adjusted_image = adjust_contrast(image, contrast=value)
+    else:
+        adjusted_image = sharpen_image(image)
     
     # Saving the enhanced image
     cv2.imwrite('enhanced_image.jpg', adjusted_image)
